27b1fe1a7de5343e2f794ac8fd976d2ada486855
narynbaev.iskander <inarynbaev@beeline.kz>
2023-11-11 20:53:11 +0600

    bubble sort + quick sort

--- algorithm.py
+++ algorithm.py
@@ -1,3 +1,4 @@
+# binary search O(log n)
 def binary_search(arr, target):
     if len(arr) <= 0:
         return None
@@ -19,4 +20,23 @@ def binary_search(arr, target):
     return -1  # Элемент не найден
 
 
+# bubble sort O(n2)
+def bubble_sort(arr):
+    arr_size = len(arr)
 
+    for i in range(arr_size):
+        for j in range(0, arr_size - i - 1):
+            if arr[j] > arr[j + 1]:
+                arr[j], arr[j + 1] = arr[j + 1], arr[j]
+    return arr
+
+
+# quick sort O(n*log n)
+def quick_sort(arr):
+    if len(arr) <= 1:
+        return arr
+
+    first_el = arr[0]
+    less = [x for x in arr[1:] if x <= first_el]
+    greater = [x for x in arr[1:] if x > first_el]
+    return quick_sort(less) + [first_el] + quick_sort(greater)
--- main.py
+++ main.py
@@ -5,3 +5,10 @@ if __name__ == '__main__':
 
     res = algorithm.binary_search(arr, 7)
     print(f'#1 Binary Search - {res}')
+
+    sort_arr = algorithm.bubble_sort(arr)
+    print(f'#2 Bubble Sort - {sort_arr}')
+
+    arr2 = [8, 4, 5, 2, 3, 7, 6, 10, 12, 34, 16, 100]
+    sort_arr2 = algorithm.quick_sort(arr2)
+    print(f'#3 Quick Sort - {sort_arr2}')
